c568d014602b6ab25aa6e7394cec571a5b9edcf3
Evanildo Leal <evanildo@wfxky.onmicrosoft.com>
2025-10-15 18:10:03 -0300

    modificação nos arquivos

--- PROJETO-UNIDADE-02/js/cadastroClientes.js
+++ PROJETO-UNIDADE-02/js/cadastroClientes.js
@@ -17,17 +17,20 @@
 const botaoSalvar = document.getElementById('salvar')
 
 
-
-
+// Função para retirar e validar os dados do formulário
 const getDados = function(){
     let nome = document.getElementById('nome')
     let email = document.getElementById('email')
+    // console.log(nome.value)
+    // console.log(email.value)
+    if(nome == ''){
+        
+    }
 
-    console.log(nome.value)
-    console.log(email.value)
 }
-    
+
+//Função de evento click para o botão Salvar
 botaoSalvar.addEventListener('click', function(){
-        getDados()
-})
+    getDados()
+});
 
