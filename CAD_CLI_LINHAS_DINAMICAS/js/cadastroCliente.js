ec3ebca44fdab912ba96a2b8f653d0e1a032d62d
EvanildoLeal <evanildo@wfxky.onmicrosoft.com>
2025-10-18 12:14:46 -0300

    alteração arquivo js

---
+++ CAD_CLI_LINHAS_DINAMICAS/js/cadastroCliente.js
@@ -0,0 +1,14 @@
+/************************************************************************************************************
+ * **********************************************************************************************************
+ * Objetivo: Manipular dados de cadastro e Listagem de dados
+ * Data: 15/10/2025
+ * Autor: Evanildo Leal
+ * versão 1.0
+ * https://github.com/EvanildoLeal/PROJETOS_WEB_PROGRAMMING.git
+ ***********************************************************************************************************/
+/*
+        var     -     Cria uma variável/objeto    (método mais antigo)
+
+        let     -       Cria uma variável/objeto de nível de escopo Local do projeto (cria dentro de uma função)
+
+        const
